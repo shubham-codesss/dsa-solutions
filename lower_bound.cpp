9632c35fe976bb023656da115e12634754b14e78
shubham-codesss <shubh.sgh1@gmail.com>
2026-07-03 15:53:53 +0530

    Create lower_bound.cpp

---
+++ lower_bound.cpp
@@ -0,0 +1,21 @@
+class Solution {
+	public:
+	int lowerBound(vector<int>& arr, int target) {
+	
+		int ans = arr.size();
+		int start = 0;
+		int end = arr.size() - 1;
+		
+		while (start <= end) {
+			int mid = end + (start - end)/2;
+			if (arr[mid] >= target) {
+				ans = mid;
+				end = mid - 1;
+			}
+			if (arr[mid]<target) {
+				start = mid + 1;
+			}
+		}
+		return ans;
+	}
+};
